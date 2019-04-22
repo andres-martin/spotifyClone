7df5c274ba441586cf76cbd07fa39290de008ddf
andres-martin <andresmartin266@gmail.com>
2019-04-22 17:36:45 -0500

    new artist created, index view modified

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,11 +3,12 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
   end
 
-  def new
-    @artist = Artist.new
-  end
-
   def show
     @artist = Artist.find(params[:id])
   end
+
+  private
+    def artist_params
+      params.require(:artist).permit(:name, :image_url, albums_attributes: [:id, :name, :image_url, :released_at]) 
+    end
 end
