a295570ede08397f450285ed779d859289272d36
andres-martin <andresmartin266@gmail.com>
2019-06-23 12:55:21 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #get 'artists/new'
   #get 'artists/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artists
+  resources :artists, only: [:index, :show]
 
   root 'artists#index', as: :artist_root
 end
